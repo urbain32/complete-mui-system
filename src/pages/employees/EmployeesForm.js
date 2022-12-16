bd94a1031cb0a60f494a85239fe706adff634244
urbain32 <urbainq32@gmail.com>
2022-12-16 18:35:40 +0200

    creating the initila values

--- src/pages/employees/EmployeesForm.js
+++ src/pages/employees/EmployeesForm.js
@@ -1,7 +1,18 @@
-import React from "react";
-
+import React, { useState } from "react";
+const initialValues = {
+  id: 0,
+  fullName: "",
+  email: "",
+  mobile: "",
+  city: "",
+  gender: "male",
+  departementId: "",
+  hireDate: new Date(),
+  isPermanent: false,
+};
 const EmployeesForm = () => {
-  return <div>EmployeesForm</div>;
+  const [values, setValues] = useState(initialValues);
+  return <form></form>;
 };
 
 export default EmployeesForm;

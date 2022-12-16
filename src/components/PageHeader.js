5485b060b10f60162561becaaed0ea32e1912b0e
urbain32 <urbainq32@gmail.com>
2022-12-16 16:24:16 +0200

    customizing div,card, paper

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,13 +1,30 @@
-import { Card, Paper } from "@mui/material";
+import { Card, Paper, styled, Typography } from "@mui/material";
 import React from "react";
-
+const Div = styled("div")(({ theme }) => ({
+  padding: theme.spacing(4),
+  display: "flex",
+  marginBottom: theme.spacing(2),
+}));
+const CardIcon = styled(Card)(({ theme }) => ({
+  padding: theme.spacing(),
+  display: "flex",
+  color: "#3c44b1",
+}));
 const PageHeader = (props) => {
   const { title, subtitle, icon } = props;
   return (
-    <Paper elevation={0} square>
-      <div>
-        <Card>{icon}</Card>
-      </div>
+    <Paper elevation={0} square sx={{ backgroundColor: "#fdfdff" }}>
+      <Div>
+        <CardIcon>{icon}</CardIcon>
+        <div>
+          <Typography variant="h6" component="div">
+            {title}
+          </Typography>
+          <Typography variant="subtitle2" component="div">
+            {subtitle}
+          </Typography>
+        </div>
+      </Div>
     </Paper>
   );
 };

bf36be7067d624eb667d11c0ba97cc835e137cee
urbain32 <urbainq32@gmail.com>
2022-12-16 11:20:36 +0200

    using style on the appbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <AppBar position="static">
+    <AppBar position="static" style={{ backgroundColor: "#fff" }}>
       <Toolbar>
         <Grid container>
           <Grid item>

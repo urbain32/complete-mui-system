297e2805c842e4e34f3bfa00d50f68d8dcc798c4
urbain32 <urbainq32@gmail.com>
2022-12-16 18:27:07 +0200

    importing the page header

--- src/pages/employees/Employees.js
+++ src/pages/employees/Employees.js
@@ -1,8 +1,19 @@
+import { PeopleOutlineTwoTone } from "@mui/icons-material";
 import React from "react";
+import PageHeader from "../../components/PageHeader";
 import EmployeesForm from "./EmployeesForm";
 
 const Employees = () => {
-  return <EmployeesForm />;
+  return (
+    <>
+      <PageHeader
+        title="New Employee"
+        subtitle="Form design with validation"
+        icon={<PeopleOutlineTwoTone fontSize="large" />}
+      />
+      <EmployeesForm />
+    </>
+  );
 };
 
 export default Employees;

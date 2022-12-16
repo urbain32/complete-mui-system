2bc7ac156f573a7909f877d4ba45aaae84922073
urbain32 <urbainq32@gmail.com>
2022-12-16 14:59:16 +0200

    creating a theme customisation

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,20 @@
-import { CssBaseline } from "@mui/material";
+import { createTheme, CssBaseline } from "@mui/material";
 import Header from "../components/Header";
 import SideMenu from "../components/SideMenu";
 import "./App.css";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#333996",
+      light: "#3c44b126",
+    },
+    secondary: {
+      main: "#f83245",
+      light: "#f8324526",
+    },
+  },
+});
 
 function App() {
   return (

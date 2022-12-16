c062b9a4d893e9d0bcd7f865556b70079d4b7c06
urbain32 <urbainq32@gmail.com>
2022-12-16 08:07:17 +0200

    styling using styled mui

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,8 +1,16 @@
 import { styled } from "@mui/system";
 import React from "react";
-
+const SideMenuDiv = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  position: "absolute",
+  left: "0px",
+  width: "320px",
+  height: "100%",
+  backgroundColor: "#253053",
+});
 const SideMenu = () => {
-  return <div className="side-menu"></div>;
+  return <SideMenuDiv></SideMenuDiv>;
 };
 
 export default SideMenu;

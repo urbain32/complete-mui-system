2286f9c7bca260f78fb8b83c9a692cc60ad38179
urbain32 <urbainq32@gmail.com>
2022-12-19 17:11:23 +0200

    adding datepicker in control

--- src/components/controls/Controls.js
+++ src/components/controls/Controls.js
@@ -2,11 +2,13 @@ import Input from "./Input";
 import RadioGroupField from "./RadioGroupField";
 import Select from "./Select";
 import CheckBox from "./CheckBox";
+import DatePicker from "./DatePicker";
 
 const Controls = {
   Input,
   RadioGroupField,
   Select,
   CheckBox,
+  DatePicker,
 };
 export default Controls;

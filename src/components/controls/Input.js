d401b3f9d23a94be73bfdcde7ad50af86bef5346
urbain32 <urbainq32@gmail.com>
2022-12-19 11:43:15 +0200

    creating a reusable textfield

---
+++ src/components/controls/Input.js
@@ -0,0 +1,17 @@
+import { TextField } from "@mui/material";
+import React from "react";
+
+export default function Input(props) {
+  const { name, label, value, onChange } = props;
+  return (
+    <TextField
+      fullWidth
+      variant="outlined"
+      label={label}
+      name={name}
+      value={value}
+      onChange={onChange}
+      sx={{ margin: 1 }}
+    />
+  );
+}

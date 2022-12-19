9a67a3d4aaaa5aea9c3b3582f94048d33aec8fac
urbain32 <urbainq32@gmail.com>
2022-12-19 15:03:56 +0200

    creating a reusable select

--- src/components/controls/Controls.js
+++ src/components/controls/Controls.js
@@ -1,8 +1,10 @@
 import Input from "./Input";
 import RadioGroupField from "./RadioGroupField";
+import Select from "./Select";
 
 const Controls = {
   Input,
   RadioGroupField,
+  Select,
 };
 export default Controls;
---
+++ src/components/controls/Select.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Select(props) {
+  const { name, label, value, onChange, options } = props;
+  return <div>Select</div>;
+}

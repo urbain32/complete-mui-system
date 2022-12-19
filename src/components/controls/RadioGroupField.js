c681d05aa61e4c4cbca65356f1f2a9200766fe66
urbain32 <urbainq32@gmail.com>
2022-12-19 15:33:40 +0200

    adding the key id

--- src/components/controls/RadioGroupField.js
+++ src/components/controls/RadioGroupField.js
@@ -14,8 +14,9 @@ export default function RadioGroupField(props) {
     <FormControl>
       <FormLabel>{label}</FormLabel>
       <RadioGroup row name={name} value={value} onChange={onChange}>
-        {items.map((item, index) => (
+        {items.map((item) => (
           <FormControlLabel
+            key={item.id}
             value={item.id}
             control={
               item.id === "female" ? (

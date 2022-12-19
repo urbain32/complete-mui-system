8d27dabbb617806d0162e7dd09c3bbe8193be59d
urbain32 <urbainq32@gmail.com>
2022-12-19 13:46:02 +0200

    creating a reusable radiogroup

---
+++ src/components/controls/RadioGroupField.js
@@ -0,0 +1,50 @@
+import {
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
+import { pink } from "@mui/material/colors";
+import React from "react";
+
+export default function RadioGroupField(props) {
+  const { name, label, value, onChange, items } = props;
+  return (
+    <FormControl>
+      <FormLabel>{label}</FormLabel>
+      <RadioGroup row name={name} value={value} onChange={onChange}>
+        {items.map((item, index) => (
+          <FormControlLabel
+            value={item.id}
+            control={
+              item.id === "female" ? (
+                <Radio
+                  sx={{
+                    "&.Mui-checked": {
+                      color: pink[600],
+                    },
+                  }}
+                />
+              ) : (
+                <Radio />
+              )
+            }
+            label={item.title}
+          />
+        ))}
+      </RadioGroup>
+    </FormControl>
+  );
+}
+// if (item.id === "female") {
+//   <FormControlLabel
+//     value={item.id}
+//     control={
+
+//     }
+//     label={item.title}
+//   />;
+// } else {
+//   <FormControlLabel value={item.id} control={<Radio />} label={item.title} />;
+// }

3990baec215497691779e6e0169517c6b772194d
urbain32 <urbainq32@gmail.com>
2022-12-19 10:19:06 +0200

    creating a handleinputchange

--- src/pages/employees/EmployeesForm.js
+++ src/pages/employees/EmployeesForm.js
@@ -17,22 +17,33 @@ const initialValues = {
 };
 const EmployeesForm = () => {
   const [values, setValues] = useState(initialValues);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
   return (
     <MyForm>
       <Grid container sx={{}}>
         <Grid item xs={6}>
           <TextField
             fullWidth
-            sx={{ marginBottom: 2, gap: 2 }}
+            sx={{ marginBottom: 2 }}
             variant="outlined"
             label="Full Name"
+            name="fullName"
             value={values.fullName}
+            onChange={handleInputChange}
           />
           <TextField
             fullWidth
             variant="outlined"
             label="Email Address"
+            name="email"
             value={values.email}
+            onChange={handleInputChange}
           />
         </Grid>
         <Grid item xs={6}></Grid>

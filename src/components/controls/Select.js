edec63ac6a0f2d33370c3ef84cea6fee336c31a0
urbain32 <urbainq32@gmail.com>
2022-12-19 15:44:55 +0200

    creating a reusavle select

--- src/components/controls/Select.js
+++ src/components/controls/Select.js
@@ -1,6 +1,24 @@
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select as MuiSelect,
+} from "@mui/material";
 import React from "react";
 
 export default function Select(props) {
   const { name, label, value, onChange, options } = props;
-  return <div>Select</div>;
+  return (
+    <FormControl variant="outlined" fullWidth>
+      <InputLabel>{label}</InputLabel>
+      <MuiSelect name={name} label={label} value={value} onChange={onChange}>
+        <MenuItem value="">None</MenuItem>
+        {options.map((item) => (
+          <MenuItem key={item.id} value={item.id}>
+            {item.title}
+          </MenuItem>
+        ))}
+      </MuiSelect>
+    </FormControl>
+  );
 }

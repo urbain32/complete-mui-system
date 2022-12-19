837bbaa229931b6f4ff2ad9bb7377c0c9dbebb47
urbain32 <urbainq32@gmail.com>
2022-12-19 16:04:31 +0200

    importing chechbox in controls

--- src/components/controls/Controls.js
+++ src/components/controls/Controls.js
@@ -1,10 +1,12 @@
 import Input from "./Input";
 import RadioGroupField from "./RadioGroupField";
 import Select from "./Select";
+import CheckBox from "./CheckBox";
 
 const Controls = {
   Input,
   RadioGroupField,
   Select,
+  CheckBox,
 };
 export default Controls;

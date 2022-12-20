c1e1b18c50b5b217c8426306d22b037e13c16b6f
urbain32 <urbainq32@gmail.com>
2022-12-20 09:40:56 +0200

    adding other prop and styling the button

--- src/components/controls/Button.js
+++ src/components/controls/Button.js
@@ -1,14 +1,18 @@
-import { Button as MuiButton } from "@mui/material";
+import { Button as Buttom, styled } from "@mui/material";
 import React from "react";
+const MuiButton = styled(Buttom)((theme) => ({
+  marginRight: 8,
+}));
 
 export default function Button(props) {
-  const { text, size, color, variant, onClick } = props;
+  const { text, size, color, variant, onClick, ...other } = props;
   return (
     <MuiButton
       variant={variant || "contained"}
       size={size || "large"}
       color={color || "primary"}
       onClick={onClick}
+      {...other}
     >
       {text}
     </MuiButton>

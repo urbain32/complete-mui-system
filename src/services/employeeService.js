0f0a242d412398713912ad8cd70be30db775265b
urbain32 <urbainq32@gmail.com>
2022-12-27 17:10:04 +0200

    creatin g a func that will generate id .

--- src/services/employeeService.js
+++ src/services/employeeService.js
@@ -1,5 +1,6 @@
 const KEYS = {
   employees: "employees",
+  employeesId: "employeesId",
 };
 
 export const getDepartementCollection = () => [
@@ -17,4 +18,12 @@ export function getAllEmployees() {
   if (localStorage.getItem(KEYS.employees) === null)
     localStorage.setItem(KEYS.employees, JSON.stringify([]));
   return JSON.parse(localStorage.getItem(KEYS.employees));
+}
+
+export function generateEmployeesId() {
+  if (localStorage.getItem(KEYS.employeesId) === null)
+    localStorage.setItem(KEYS.employeesId, "0");
+  var id = parseInt(localStorage.getItem(KEYS.employeesId));
+  localStorage.setItem(KEYS.employeesId, (++id).toString());
+  return id;
 }

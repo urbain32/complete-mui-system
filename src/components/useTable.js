6be1de2c689105799d90045b7df094ba0e2eb43b
urbain32 <urbainq32@gmail.com>
2022-12-29 07:39:13 +0200

    creating a table container function

--- src/components/useTable.js
+++ src/components/useTable.js
@@ -2,5 +2,6 @@ import { Table } from "@mui/material";
 import React from "react";
 
 export default function useTable(records, headCell) {
-  return {};
+  const TblContainer = (props) => <Table></Table>;
+  return { TblContainer };
 }

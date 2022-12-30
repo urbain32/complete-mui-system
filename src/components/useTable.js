900f31b7eef341e376e00baf70b695bc902abccc
urbain32 <urbainq32@gmail.com>
2022-12-30 17:21:44 +0200

    maping our TblHeadeer

--- src/components/useTable.js
+++ src/components/useTable.js
@@ -1,10 +1,18 @@
-import { Table, TableHead } from "@mui/material";
+import { Table, TableCell, TableHead, TableRow } from "@mui/material";
 import React from "react";
 
-export default function useTable(records, headCell) {
+export default function useTable(records, headCells) {
   const TblContainer = (props) => <Table>{props.children}</Table>;
   const TblHead = (props) => {
-    return <TableHead></TableHead>;
+    return (
+      <TableHead>
+        <TableRow>
+          {headCells.map((headCell) => (
+            <TableCell key={headCell.id}>{headCell.label}</TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+    );
   };
   return { TblContainer, TblHead };
 }

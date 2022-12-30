ac18bbb85bfbc859cde0c706263b008822aee81a
urbain32 <urbainq32@gmail.com>
2022-12-30 17:52:37 +0200

    adding margin

--- src/components/useTable.js
+++ src/components/useTable.js
@@ -1,11 +1,14 @@
+import { ThemeContext } from "@emotion/react";
 import { Table, TableCell, TableHead, TableRow } from "@mui/material";
 import React from "react";
 
-export default function useTable(records, headCells) {
-  const TblContainer = (props) => <Table>{props.children}</Table>;
+export default function useTable(records, headCells, theme) {
+  const TblContainer = (props) => (
+    <Table sx={{ marginTop: "24px" }}>{props.children}</Table>
+  );
   const TblHead = (props) => {
     return (
-      <TableHead>
+      <TableHead sx={{ fontWeight: "600" }}>
         <TableRow>
           {headCells.map((headCell) => (
             <TableCell key={headCell.id}>{headCell.label}</TableCell>

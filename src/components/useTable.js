7e397de392c90bbf2f3178879de3ae35605ce975
urbain32 <urbainq32@gmail.com>
2022-12-30 07:56:37 +0200

    fo rour data to be view we pass them as props

--- src/components/useTable.js
+++ src/components/useTable.js
@@ -2,6 +2,6 @@ import { Table } from "@mui/material";
 import React from "react";
 
 export default function useTable(records, headCell) {
-  const TblContainer = (props) => <Table></Table>;
+  const TblContainer = (props) => <Table>{props.children}</Table>;
   return { TblContainer };
 }

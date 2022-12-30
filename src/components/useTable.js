f94838556e2aefabc6063aa9fd4d5999c5e62411
urbain32 <urbainq32@gmail.com>
2022-12-30 17:05:42 +0200

    creating a reusable tableHead

--- src/components/useTable.js
+++ src/components/useTable.js
@@ -1,7 +1,10 @@
-import { Table } from "@mui/material";
+import { Table, TableHead } from "@mui/material";
 import React from "react";
 
 export default function useTable(records, headCell) {
   const TblContainer = (props) => <Table>{props.children}</Table>;
-  return { TblContainer };
+  const TblHeader = (props) => {
+    return <TableHead></TableHead>;
+  };
+  return { TblContainer, TblHeader };
 }

544cbe37f1853ed8fd1127795349fc4e25555f68
urbain32 <urbainq32@gmail.com>
2023-01-02 08:00:54 +0200

    styling our table

--- src/components/useTable.js
+++ src/components/useTable.js
@@ -1,11 +1,23 @@
-import { ThemeContext } from "@emotion/react";
-import { Table, TableCell, TableHead, TableRow } from "@mui/material";
+import { styled, Table, TableCell, TableHead, TableRow } from "@mui/material";
 import React from "react";
+const MyTable = styled(Table)(({ theme }) => ({
+  margin: theme.spacing(3), //spacing(1)=8px
+  "& thead th": {
+    fontWeight: "600",
+    color: theme.palette.primary.main,
+    backgroundColor: theme.palette.primary.light,
+  },
+  "& tbody td": {
+    fontWeight: "300",
+  },
+  "& tbody tr:hover": {
+    backgroundColor: "#fffbf2",
+    cursor: "pointer",
+  },
+}));
 
 export default function useTable(records, headCells, theme) {
-  const TblContainer = (props) => (
-    <Table sx={{ marginTop: "24px" }}>{props.children}</Table>
-  );
+  const TblContainer = (props) => <MyTable>{props.children}</MyTable>;
   const TblHead = (props) => {
     return (
       <TableHead sx={{ fontWeight: "600" }}>

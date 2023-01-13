0197561a80810127a4c2ee3a20a20e45cee5b6ea
urbain32 <urbainq32@gmail.com>
2023-01-13 09:20:22 +0200

    condition if a headcell has

--- src/components/useTable.js
+++ src/components/useTable.js
@@ -47,15 +47,19 @@ export default function useTable(records, headCells, theme) {
               key={headCell.id}
               sortDirection={orderBy === headCell.id ? order : false}
             >
-              <TableSortLabel
-                active={orderBy === headCell.id}
-                direction={orderBy === headCell.id ? order : "asc"}
-                onClick={() => {
-                  handleSort(headCell.id);
-                }}
-              >
-                {headCell.label}
-              </TableSortLabel>
+              {headCell.disableSorting ? (
+                headCell.label
+              ) : (
+                <TableSortLabel
+                  active={orderBy === headCell.id}
+                  direction={orderBy === headCell.id ? order : "asc"}
+                  onClick={() => {
+                    handleSort(headCell.id);
+                  }}
+                >
+                  {headCell.label}
+                </TableSortLabel>
+              )}
             </TableCell>
           ))}
         </TableRow>

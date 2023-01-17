412999b90151ff58138d8c84c7f86c64efd2a90f
urbain32 <urbainq32@gmail.com>
2023-01-17 08:33:08 +0200

    Other other to our reausable Input

--- src/components/controls/Input.js
+++ src/components/controls/Input.js
@@ -2,7 +2,7 @@ import { TextField } from "@mui/material";
 import React from "react";
 
 export default function Input(props) {
-  const { name, label, value, onChange, error = null } = props;
+  const { name, label, value, onChange, error = null, ...other } = props;
   return (
     <TextField
       fullWidth
